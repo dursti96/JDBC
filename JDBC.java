a6dd086b81c8b1b28dad5b77a1ec38b454c2f65e
dursti96 <86721188+dursti96@users.noreply.github.com>
2021-10-01 08:53:58 +0200

    Add files via upload

--- JDBC.java
+++ JDBC.java
@@ -10,18 +10,21 @@ public class JDBC {
         database database1 = new database("alex", "asdf", "jdbc");
 
 
-
         try {
-            //database1.SQLSelectAll("user");
-            database1.SQLInsert("user",2,"a","a","a");
+            System.out.println(database1.SQLSelectCountAll("test"));
+            System.out.println(database1.SQLCheckAutoIncrement("test"));
+            System.out.println(database1.SQLInsert("test", "username", "20220311", "22"));
+            String [][] selectAll = database1.SQLSelectAll("test");
+
+            for(int i = 0; i < selectAll.length; i++){
+                for(int a = 0; a < selectAll[0].length; a++){
+                    System.out.print(selectAll[i][a] + "    ");
+                }
+                System.out.println();
+            }
         } catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(JDBC.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-
     }
-
-
-
-
 }
--- database.java
+++ database.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.ArrayList;
 
 public class database {
 
@@ -14,41 +15,126 @@ public class database {
     }
 
 
-    /** select * from ?; println result; only for tables with 4 columns */
-    public void SQLSelectAll(String tableName) throws SQLException, ClassNotFoundException {
+
+    /** select * from ?; return values are Strings */
+    public String[][] SQLSelectAll(String tableName) throws SQLException, ClassNotFoundException {
 
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + this.dBName, this.dBUserName, this.dBPassword);
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery("select * from " + tableName);
-        StringBuilder stringBuilder = new StringBuilder();
+
+        int rowCount = SQLSelectCountAll(tableName);
+        int columnCount = resultSet.getMetaData().getColumnCount();
+        String[][] result = new String[rowCount][columnCount];
+        int a = 0;  // row number
+
         while(resultSet.next()) {
-            stringBuilder.append(resultSet.getInt(1)).append(" ");
-            stringBuilder.append(resultSet.getString(2)).append(" ");
-            stringBuilder.append(resultSet.getString(3)).append(" ");
-            stringBuilder.append(resultSet.getString(4 )).append(" ").append("\n");
+            for(int i = 0; i < columnCount; i++){
+
+                result[a][i] = resultSet.getObject(i+1).toString();
+            }
+            a++;
         }
-        System.out.println(stringBuilder);
+        return result;
     }
 
 
-    /** insert into table; values need to be in the right order; only tables with 4 values */
-    public void SQLInsert(String tableName, int value1, String value2, String value3, String value4) throws SQLException, ClassNotFoundException {
+    /** select count All */
+    public Integer SQLSelectCountAll(String tableName) throws SQLException, ClassNotFoundException {
 
-        // first query for column names
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + this.dBName, this.dBUserName, this.dBPassword);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select count(*) from " + tableName);
+
+        //get result
+        resultSet.next();
+        Integer result = resultSet.getInt("count(*)");
+
+        return result;
+    }
+
+
+    /** get names of columns from table */
+    private ArrayList<String> SQLGetColumnNames(String tableName) throws SQLException, ClassNotFoundException {
+
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + "INFORMATION_SCHEMA", this.dBUserName, this.dBPassword);
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + tableName + "' AND TABLE_SCHEMA = '" + this.dBName + "' ORDER BY ORDINAL_POSITION");
 
-        StringBuilder stringBuilder = new StringBuilder();
+        ArrayList<String> result = new ArrayList<>();
+
         while(resultSet.next()) {
-
-            stringBuilder.append(resultSet.getString(1 )).append("\n");
+            result.add(resultSet.getString("COLUMN_NAME"));
         }
-        System.out.println(stringBuilder);
 
-        // second query, put in column names:
+        return result;
     }
+
+    /** get column names of all columns where auto increment is on */
+    public ArrayList<String> SQLCheckAutoIncrement(String tableName) throws SQLException, ClassNotFoundException {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + "INFORMATION_SCHEMA", this.dBUserName, this.dBPassword);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + tableName + "' AND TABLE_SCHEMA = '" + this.dBName + "' AND EXTRA like '%auto_increment%' ORDER BY ORDINAL_POSITION");
+
+        ArrayList<String> result = new ArrayList<>();
+
+        while(resultSet.next()) {
+            result.add(resultSet.getString("COLUMN_NAME"));
+        }
+
+        return result;
+    }
+
+
+    /** insert into tableName values (?,?,?,...); Integers,Dates,... have to be written as String; Date-Format: "YYYYMMDD"
+     * if a column is auto increment, no parameter is needed; example:
+     * if column names: "ID", "Username", "Password"; "ID" auto increments; then SQLInsert(example-username, example-password)*/
+    public boolean SQLInsert(String tableName, String ... data) throws SQLException, ClassNotFoundException {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + this.dBName, this.dBUserName, this.dBPassword);
+
+        // get prepared statement with varying number of parameters
+        String sql = "insert into " + tableName + " (";
+        ArrayList<String> columnNames = SQLGetColumnNames(tableName);
+
+        // add column names to prepared statement, except auto increment columns
+        for (int i = 0; i < columnNames.size() -1; i++){
+            if(!SQLCheckAutoIncrement(tableName).contains(columnNames.get(i))){
+                sql = sql + columnNames.get(i) + ", ";
+            }
+        }
+
+        sql = sql + columnNames.get(columnNames.size()-1);
+        sql = sql + ") values (";
+
+        // add placeholder for insert values to prepared statement
+        for(int i = 0; i < data.length-1; i++){
+            sql = sql + "?,";
+        }
+        sql = sql + "?)";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        // set parameters of data
+        for(int i = 0; i < data.length; i++){
+            preparedStatement.setString(i+1,data[i]);
+        }
+
+        int rowsUpdated = preparedStatement.executeUpdate();
+
+        if(rowsUpdated == 0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
 
 }
